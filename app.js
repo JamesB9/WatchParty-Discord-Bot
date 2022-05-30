a0ddf8db12382c7abaedc06ce450b336551fded9
J-B <57415099+JamesB9@users.noreply.github.com>
2022-05-30 20:20:37 +0100

    Ping/Pong with event handlers

--- app.js
+++ app.js
@@ -14,30 +14,24 @@ DeployCommands();
 client.commands = new Discord.Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-  const command = client.commands.get(interaction.commandName);
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-  }
-});
-
-client.once('ready', () => {
-	console.log('Ready!');
-});
 
 client.login(process.env.BOT_TOKEN)
